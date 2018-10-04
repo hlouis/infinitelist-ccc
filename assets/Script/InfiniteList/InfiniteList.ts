8066ade3186c8eb5335af13917da134d0aa66eae
Louis Huang <me@hlouis.com>
2018-10-04 14:55:07 +0800

    Add basic property to list

---
+++ assets/Script/InfiniteList/InfiniteList.ts
@@ -0,0 +1,93 @@
+const { ccclass, property } = cc._decorator;
+
+enum Direction {
+	vertical = 1,
+	horizontal,
+}
+
+interface GetCellNumber {
+	(): number;
+}
+
+interface GetCellSize {
+	(dataIndex:number): number;
+}
+
+interface GetCellView {
+	(dataIndex:number, cellIndex:number): cc.Node;
+}
+interface InitParam {
+	getCellNumber: GetCellNumber,
+	getCellSize: GetCellSize,
+	getCellView: GetCellView,
+}
+
+@ccclass
+export default class InfiniteList extends cc.Component {
+	@property({
+		type: cc.Enum(Direction),
+		tooltip: "List 滚动的方向，可以选择垂直或者水平"
+	})
+	public direction = Direction.vertical;
+
+	@property({
+		tooltip: "cell 之间的像素间隔，最开始和最后面不会添加"
+	})
+	public spacing = 0;
+
+	public Init(p:InitParam) {
+		this._init(p);
+	}
+
+	/**
+	 * Reload 整个 List，这时获取数据的回调函数会重新触发一遍，所有的 cell 也会更新一遍内容
+	 */
+	public Reload() {
+	}
+
+	/**
+	 * 重新刷新当前显示 cell 的内容，不会重新载入整个列表，所以如果列表的数据数量发生了变化，
+	 * 调用 Refresh 是没有用处的，请调用 Reload
+	 */
+	public Refresh() {
+	}
+
+	////////////////////////////////////////////////////////////
+	// Implenmentions
+	////////////////////////////////////////////////////////////
+
+	private _scrollView:cc.ScrollView;
+	private _content:cc.Node;
+	private _delegate:InitParam;
+
+	private _init(p:InitParam) {
+		this._delegate = p;
+	}
+
+	public onLoad() {
+		// setup scrollview component
+		this._scrollView = this.node.getComponent(cc.ScrollView);
+		if (!this._scrollView) {
+			this._scrollView = this.node.addComponent(cc.ScrollView);
+			if (this.direction == Direction.horizontal) {
+				this._scrollView.vertical = false;
+				this._scrollView.horizontal = true;
+			} else {
+				this._scrollView.vertical = true;
+				this._scrollView.horizontal = false;
+			}
+		}
+
+		// setup content node(which is root of every cell)
+		this._content = new cc.Node();
+		this.node.addChild(this._content);
+		const layout = this._content.addComponent(cc.Layout);
+		if (this.direction == Direction.horizontal) {
+			layout.type = cc.Layout.Type.HORIZONTAL;
+			layout.spacingX = this.spacing;
+		} else {
+			layout.type = cc.Layout.Type.VERTICAL;
+			layout.spacingY = this.spacing;
+		}
+	}
+}
